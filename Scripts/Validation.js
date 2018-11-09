3db29a96ac38d65b5d510602abbf7a5c91d71b4e
Brian <bmwahler@gmail.com>
2018-11-09 09:55:49 -0500

    Lab22

--- Scripts/Validation.js
+++ Scripts/Validation.js
@@ -1,5 +1,18 @@
-﻿var fnRegEx = [a - zA - Z]{ 2, };
-var fnRegEx = [a - zA - Z]{ 2, };
-var eaRegEx = ([0 - 9A - z]{ 5, 30})(@[0 - 9A - z]{ 5, 10 }).([0 - 9A - z]{ 2, 3 });
-var pnRegEx = (\d{ 3})-(\d{ 3 }) -(\d{ 4 });
-var pRegEx = [a - zA - Z]\w{ 3, 14};
+﻿var fnRegEx = /^[a-zA-Z]{2,}$/;
+var fnRegEx = /^[a-zA-Z]{2,}$/;
+var eaRegEx = /"^[A-z0-9]{5,30}@[a-z]{5,10}.[a-z]{2,3}$"/;
+var pnRegEx = /(\d{ 3})-(\d{ 3 })-(\d{ 4 })/;
+var pRegEx = /^[a-zA-Z]{2,}$/;
+
+
+function Validate()
+{
+    if (/^[a-zA-Z]{2,}$/ || /^[a-zA-Z]{2,}$/ || /"^[A-z0-9]{5,30}@[a-z]{5,10}.[a-z]{2,3}$"/ || /(\d{ 3})-(\d{ 3 })-(\d{ 4 })/ || /^[a-zA-Z]{2,}$/)
+    {
+        return true;
+    }
+    else
+    {
+        return ("There are missed requirements");
+    }
+}
